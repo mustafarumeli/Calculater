474da5b503ea782a9cbad1b933e69ff9183175cd
Mustafa Rumeli <mustafarumeli@outlook.com>
2020-12-25 20:38:24 +0300

    imageupload service

---
+++ Calculater.ImageUpload/Controllers/ImageUploadController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Calculater.ImageUpload.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageUploadController : ControllerBase
+    {
+        [HttpGet("Upload")]
+        public IActionResult Upload()
+        {
+            string fileName = Guid.NewGuid().ToString().Replace("-", "_") + ".png"; ;
+            return Ok(fileName);
+        }
+    }
+}
