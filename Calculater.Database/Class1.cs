7454d01f2676d233d673892ec9f715279c950e33
Mustafa Rumeli <mustafarumeli@outlook.com>
2020-12-25 21:01:41 +0300

    database-delete

---
+++ Calculater.Database/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Calculater.Models;
+
+namespace Calculater.Database
+{
+    public class Database
+    {
+        public IEnumerable<ImageFile> ImageFiles =>
+            new List<ImageFile>()
+            {
+                new ImageFile() {FileName = "image1.png"},
+                new ImageFile() {FileName = "image2.png"},
+                new ImageFile() {FileName = "image3.png"},
+                new ImageFile() {FileName = "image4.png"},
+            }.AsEnumerable();
+    }
+}
