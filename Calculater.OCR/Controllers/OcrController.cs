4be25f6ffe106362433b7ee28c091a73fea1b27b
Mustafa Rumeli <mustafarumeli@outlook.com>
2020-12-25 20:50:29 +0300

    ocr

---
+++ Calculater.OCR/Controllers/OcrController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Calculater.OCR.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OCRController : ControllerBase
+    {
+        [HttpGet("OCR")]
+        public IActionResult Get(string fileName)
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
