9bd24fb84f4dfa0f7fa6512e7e30f8c2af5f1d60
Mustafa Rumeli <mustafarumeli@outlook.com>
2020-12-27 15:35:22 +0300

    rabbit console

---
+++ Calculater.RabbitConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Calculater.RabbitConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
